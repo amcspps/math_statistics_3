fedc9fc6ea4524bb9298ca507db163f3fd722f33
kchnms <pashafizmat1@gmail.com>
2023-06-02 00:10:45 +0300

    final

---
+++ lab10/src/data.m
@@ -0,0 +1,15 @@
+pkg load interval
+
+input = csvread("Channel_1_700nm_0.23mm.csv")
+eps = 1e-4
+
+input_int = infsup(input - eps, input + eps)
+
+errorbar(mid(input_int), rad(input_int), "b")
+title("Data")
+xlabel("n")
+ylabel("mV")
+xlim([0, size(input, 1)])
+
+print -dpdfcrop ../figures/data_int.pdf
+
